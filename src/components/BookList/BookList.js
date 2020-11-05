5fb90810baf02051117a765c5a3d617d40d2831f
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-11-05 16:29:01 -0600

    Implement BookList component.

---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const BookList = () => {
+  return (
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </table>
+  );
+};
+
+BookList.propTypes = {};
+
+export default BookList;
