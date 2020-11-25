d37a58684f368de11156369dd41ac6f9678f3ed2
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-11-24 22:34:22 -0600

    Add uuid to addBook action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
 import { DELETE_BOOK, CREATE_BOOK } from '../utils/const';
 
 export const addBook = ({ book, category }) => ({
   type: CREATE_BOOK,
-  payload: { id: book + Math.random(), title: book, category },
+  payload: { id: uuidv4(), title: book, category },
 });
 export const removeBook = book => ({ type: DELETE_BOOK, payload: book });
